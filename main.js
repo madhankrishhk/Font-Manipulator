f37530fa3c88eb58b152728494467af158b0d9cf
madhankrishhk <84235355+madhankrishhk@users.noreply.github.com>
2022-04-15 16:55:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(550,500);
+    canvas = createCanvas(550,550);
+    canvas.position(560,100);
+    poseNet =ml5.poseNet(video, modalLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function modalLoaded(){
+    console.log("poseNet Is Initialized");
+}
+function gotPoses(results){
+if(results.length>0){
+    console.log(results);
+}
+}
